d2da5b7c95c1a8e68bb2f6f0538cbb623ffd6515
jeremy-zou <jeremy-zou@users.noreply.github.com>
2020-03-04 21:28:29 -0500

    Added comments for readability

--- index.js
+++ index.js
@@ -4,20 +4,20 @@ var svg = d3.select("div#container").append("svg")
     .attr("preserveAspectRatio", "xMinYMin meet")
     .attr("viewBox", "0 0 " + width + " " + height)
     .classed("svg-content", true);
-
+    
+// Scales and centers map
 var projection = d3.geoMercator().translate([width/2, height/2]).scale(6000).center([-83.5,32.7]);
 var path = d3.geoPath().projection(projection);
 
 var map = d3.json("map_data/ga_counties.json");
-//var map = d3.json("map_data/test.geojson");
-var counties = d3.csv("map_data/ga_counties_coordinates.csv");
-
+// Popup for county name when hovering
 var tooltip = d3.select("body").append("div") 
     .attr("class", "tooltip")       
     .style("opacity", 0);
 
-Promise.all([map, counties]).then(function(values){    
-    // draw map
+// Promise allows multiple iterables to be passed through
+Promise.all([map]).then(function(values){    
+    // draws map
     svg.selectAll("path")
         .data(values[0].features)
         .enter()
